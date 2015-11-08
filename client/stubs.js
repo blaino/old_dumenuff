dcb027028ab94bad14791a3558cd41e669b997ca
Blaine Nelson <blaine.m.nelson@gmail.com>
2015-11-08 06:47:12 -0500

    Comment out stub (until you understand it better).

--- client/stubs.js
+++ client/stubs.js
@@ -1,9 +1,9 @@
 // For client-side response prediction
 
-Meteor.methods({
-    newMessage: function (userId, message) {
-  	message.timestamp = Date.now();
-        message.user = userId;
-        Messages.insert(message);
-    }
-});
+// Meteor.methods({
+//     newMessage: function (userId, message) {
+//   	message.timestamp = Date.now();
+//         message.user = userId;
+//         Messages.insert(message);
+//     }
+// });
