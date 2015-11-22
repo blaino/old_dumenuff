ee8ff627c2892b9da82c76512e5459b9fb971885
Blaine Nelson <blaine.m.nelson@gmail.com>
2015-11-22 16:46:03 -0500

    Seem to reliably get to joining game.

--- tests/casper/player2.js
+++ tests/casper/player2.js
@@ -4,24 +4,36 @@ var casper = require('casper').create({
     userAgent: 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.2 Safari/537.36'
 });
 
+casper.options.viewportSize = {width: 700, height: 600};
+
 casper.start('http://localhost:3000');
 
+casper.wait(1000);
 
 casper.then(function () {
     // if logged in, log out before logging in
+    this.capture('catpure.png');
     if (this.exists('#login-name-link')) {
+        this.echo('==== found #login-name-link');
         this.click('#login-name-link');
-    } else if (this.exists('#login-sign-in-link')) {
-        this.click('#login-sign-in-link');
-    } else {
-        this.echo('neither #login-name-link nor #login-sign-in-link found, capturing');
-        this.capture('sign-in-error.png');
+        this.waitForSelector('#login-buttons-logout', function () {
+            this.click('#login-buttons-logout');
+            this.capture('afterLogout.png');
+        });
     }
 });
 
+casper.then(function () {
+    this.waitForSelector('#login-sign-in-link', function () {
+        this.echo('==== found #login-sign-in-link');
+        this.click('#login-sign-in-link');
+    });
+});
+
 casper.then(function() {
+    this.echo('==== going to wait for username/password field');
     this.waitForSelector('#login-buttons-password', function () {
-        this.echo('See #login-buttons-password');
+        this.echo('==== See #login-buttons-password');
     });
 });
 
@@ -35,13 +47,49 @@ casper.thenEvaluate(function(username, password) {
 casper.thenClick('#login-buttons-password');
 
 casper.then(function () {
-    this.capture('capture.png');
-
     this.waitForText("gunnar", function () {
-        this.echo('Found gunnar');
+        this.echo('==== Found gunnar');
         this.capture('withGunnar.png');
     });
 });
 
+casper.then(function () {
+    if ("button[id='#start-button']:enabled") {
+        this.echo('==== found enabled start button');
+        this.click('#start-button');
+    }
+});
+
+
+casper.then(function () {
+    this.waitForText("Waiting", function () {
+        this.echo('==== Found Waiting');
+        this.capture('foundWaiting.png');
+    });
+});
+
+
+casper.then(function () {
+    //this.waitForSelector("button[id='#join-button']:enabled", function () {
+    this.waitForSelector("#join-button", function () {
+        this.echo('==== found join button');
+        this.click('#join-button');
+        this.capture('join1.png');
+    });
+});
+
+casper.then(function () {
+    this.waitForText("Game ends in",
+                     function () {
+                         this.echo('==== Found Game ends in');
+                         this.click('#bot-button');
+                         this.capture('score2.png');
+                     },
+                     function () {
+                         this.echo('==== Cannot find Game ends in');
+                         this.capture('errorScore2.png');
+                     },
+                     6000);
+});
 
 casper.run();
