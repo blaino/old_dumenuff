71e2a328f84ca68f03821ddc7f83f26b28a4e4bd
Blaine Nelson <blaine.m.nelson@gmail.com>
2015-12-05 16:35:42 -0500

    Refactor: splash or play

--- client/App.jsx
+++ client/App.jsx
@@ -6,29 +6,42 @@ const ThemeManager = new mui.Styles.ThemeManager();
 
 App = React.createClass({
 
-    renderFooter() {
-        var user = Meteor.userId();
+    mixins: [ReactMeteorData],
+
+    getMeteorData() {
+        return {
+            game: Game.findOne({}),
+        }
+    },
+
+    renderSplash() {
         return [
+            <h1 className="title">dumenuff</h1>,
+            <InstructionBox />,
             <AccountsUIWrapper />,
             <SplashFooter />
         ]
     },
 
+    renderPlay() {
+        return <Play />
+    },
+
     render() {
+        var game = this.data.game;
+        var renderFunction = this.renderSplash;
+
+        if (game) {
+            var state = game.state;
+
+            if (state == 'Started') {
+                renderFunction = this.renderPlay;
+            }
+        }
+
         return (
             <div className="outer">
-
-                <h1 className="title">dumenuff</h1>
-
-                <InstructionBox />
-
-                {this.renderFooter()}
-
-                <div className="splashFooter">
-                    <p>--------- Next Page -------</p>
-                </div>
-
-                <Play/>
+                {renderFunction()}
             </div>
         );
     }
