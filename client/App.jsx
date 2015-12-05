33b4f378ced2cb8f1d14d8f2ef60e5321f0aa69b
Blaine Nelson <blaine.m.nelson@gmail.com>
2015-12-05 16:22:11 -0500

    Refactor; add InstructionBox, SplashFooter

--- client/App.jsx
+++ client/App.jsx
@@ -4,105 +4,14 @@ const {
 
 const ThemeManager = new mui.Styles.ThemeManager();
 
-// App component - represents the whole app
 App = React.createClass({
 
-    // This mixin makes the getMeteorData method work
-    mixins: [ReactMeteorData],
-
-    // Loads items from the Tasks collection and puts them on this.data.tasks
-    getMeteorData() {
-        return {
-            game: Game.findOne({}),
-            playerInWaiting: Waiting.findOne({player: Meteor.userId()}),
-        }
-    },
-    /* getInitialState() {}, */
-
-    childContextTypes: {
-        muiTheme: React.PropTypes.object
-    },
-
-    getChildContext: function() {
-        return {
-            muiTheme: ThemeManager.getCurrentTheme()
-        };
-    },
-
-    gameState: function () {
-        var game = this.data.game;
-        if (game) {
-            var waitingFor = game.numPlayers - game.numReady;
-            var state = game.state;
-            if (state == "Readying") {
-                return "Game starts in " + game.readyTime + " seconds";
-            } else if (state == "Started") {
-                if (Meteor.userId()) {
-                    Meteor.call('findRoom', Meteor.userId(), function (error, room) {
-                        if (error) {
-                            Session.set('channel', 'lobby');
-                        } else {
-                            Session.set('channel', room._id);
-                        };
-                    });
-                };
-                return "Game ends in " + game.gameTime + " seconds";
-            } else if (state == "Ended") {
-                return "Game over";
-            } else {
-                return "Waiting for " + waitingFor + " players";
-            }
-        } else {
-            return "Waiting for x players";
-        }
-    },
-
-    joinButtonDisabled: function () {
-        var game = this.data.game;
-        if (game) {
-            var state = game.state;
-            if (state != "Waiting") {
-                return true;
-            };
-        };
-
-        var playerInWaiting = this.data.playerInWaiting;
-        if (playerInWaiting) {
-            return true;
-        }
-    },
-
-    startButtonDisabled: function () {
-        var game = this.data.game;
-
-        if (game) {
-            var state = game.state;
-            if (state != "Ended") {
-                return true;
-            };
-        };
-    },
-
-    clickJoinButton: function () {
+    renderFooter() {
         var user = Meteor.userId();
-        if (user) {
-            Meteor.call('addPlayer', user);
-            Meteor.call('checkReady', function (error, isReady) {
-                if (isReady) {
-                    Meteor.call('readyGame');
-                };
-            });
-        }
-    },
-
-    clickStartButton: function () {
-        var readyTime = Meteor.settings.public.readyTime,
-            gameTime = Meteor.settings.public.gameTime,
-            numPlayers = Meteor.settings.public.numPlayers,
-            percentBot = Meteor.settings.public.percentBot;
-
-        Meteor.call('cleanUp');
-        Meteor.call('newGame', readyTime, gameTime, numPlayers, percentBot);
+        return [
+            <AccountsUIWrapper />,
+            <SplashFooter />
+        ]
     },
 
     render() {
@@ -111,25 +20,13 @@ App = React.createClass({
 
                 <h1 className="title">dumenuff</h1>
 
-                <AccountsUIWrapper />
+                <InstructionBox />
 
-                <div className="subtitle">{this.gameState()}</div>
+                {this.renderFooter()}
 
-                <RaisedButton
-                    disabled={this.joinButtonDisabled()}
-                    onClick={this.clickJoinButton}
-                    style={{float: "left",
-                            margin: "10px"}}
-                    label="Join"
-                    primary={true}/>
-
-                <RaisedButton
-                    disabled={this.startButtonDisabled()}
-                    onClick={this.clickStartButton}
-                    style={{float: "left",
-                            margin: "10px"}}
-                    label="Start"
-                    primary={true}/>
+                <div className="splashFooter">
+                    <p>--------- Next Page -------</p>
+                </div>
 
                 <Play/>
             </div>
---
+++ client/components/InstructionBox.jsx
@@ -0,0 +1,62 @@
+InstructionBox = React.createClass({
+
+    mixins: [ReactMeteorData],
+
+    getMeteorData() {
+        return {
+            game: Game.findOne({}),
+        }
+    },
+
+    renderTeaser() {
+        return (
+            <div className="instructions">
+                <p>You: Are you a bot?</p>
+                <p>Player: I know you are but what am I?</p>
+                <p>You: I think you're a bot.</p>
+                <p>Player: I think you suck</p>
+            </div>
+        );
+    },
+
+    renderInstructions() {
+        return (
+            <div className="instructions">
+                <p>Chat with your opponent. If you think your opponent is a
+                    bot click the bot button. If you think your oppoent is a
+                    human cilck the human button. You get 1 point if you're
+                    right; you'll lose 2 if you're wrong.
+                </p>
+            </div>
+        );
+    },
+
+    renderLeaderBoard() {
+        return (
+            <div className="instructions">
+                <p>Joe won!</p>
+            </div>
+        );
+    },
+
+    render() {
+        var game = this.data.game;
+        var renderFunction = this.renderTeaser;
+
+        if (game) {
+            var state = game.state;
+
+            if (state == 'Ended') {
+                renderFunction = this.renderLeaderBoard;
+            } else if (state == 'Waiting' || state == 'Readying') {
+                renderFunction = this.renderInstructions;
+            }
+        }
+
+        return (
+            <div>
+                {renderFunction()}
+            </div>
+        );
+    }
+});
