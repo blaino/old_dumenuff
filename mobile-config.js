c6516964751aa409930332115b0b5c097d84f5bc
Blaine Nelson <blaine.m.nelson@gmail.com>
2015-12-20 10:42:22 -0500

    Placeholder icons and config for ios.

---
+++ mobile-config.js
@@ -0,0 +1,16 @@
+App.info({
+  name: 'dumenuff',
+  description: 'A chat game built with Meteor',
+  version: '0.0.1'
+});
+
+App.icons({
+  'iphone': 'resources/icons/ChatterBox16.png',
+  'iphone_2x': 'resources/icons/ChatterBox32.png'
+});
+
+App.launchScreens({
+  'iphone': 'resources/splash/ChatterBox.png',
+  'iphone_2x': 'resources/splash/ChatterBoxFeatured.png',
+  'iphone5': 'resources/splash/ChatterBoxFeatured.png'
+});
